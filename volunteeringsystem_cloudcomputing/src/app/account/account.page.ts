df2287c7dabf19da9a534075c34f36235261891c
shireenabraham <shireenabraham@gmail.com>
2019-04-28 22:32:55 -0700

    Update account.page.ts

--- volunteeringsystem_cloudcomputing/src/app/account/account.page.ts
+++ volunteeringsystem_cloudcomputing/src/app/account/account.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ShareService } from '../services/share.service';
+import { UserService } from '../services/user.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { User } from '../Models/user';
 
 @Component({
   selector: 'app-account',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountPage implements OnInit {
 
-  constructor() { }
+   private user: any;
+  
+  constructor(private afAuth: AngularFireAuth, private shareService: ShareService, private userService: UserService) { }
 
   ngOnInit() {
+  	this.afAuth.authState.subscribe(d => {
+      
+      	this.getUser(d.uid);
+      	
+    });
+  }
+
+  async getUser(userId) {
+  	this.user = await this.userService.getUser(userId);
+  	
   }
 
 }
+
